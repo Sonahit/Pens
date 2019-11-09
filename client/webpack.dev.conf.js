04c7cf97d7406ef7be8b23f5187419279f7202e1
Sonahit <grandpajok@gmail.com>
2019-11-09 16:47:13 +0600

    Fixed developmed webpack

--- client/webpack.dev.conf.js
+++ client/webpack.dev.conf.js
@@ -1,10 +1,8 @@
 const merge = require("webpack-merge");
 const base = require("./webpack.base.conf");
-const webpack = require("webpack");
 
 const dev = merge(base, {
   mode: "development",
-  plugins: [new webpack.HotModuleReplacementPlugin()],
   devtool: "eval-source-map"
 });
 
